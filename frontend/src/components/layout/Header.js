06dd71cc72173401a3df421e657a3e912a9f9a86
Richard <rsmith37@ggc.edu>
2019-09-18 20:08:03 -0400

    changed nav links to buttons

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom';
 
 const Header = props => {
   return (
-    <nav className="navbar navbar-expand-sm mb-3 py-0">
-      <div className="logo">
+    <nav className="navbar sticky-top navbar-expand-sm mb-3 py-0">
+      <div className="logo" style={{ height: '50vh' }}>
 
       </div>
-      <div className="container">
+      <div className="container-fluid bg-danger">
         <h1>Welcome!</h1>
         <div>
            <ul className="navbar-nav mr-auto">
              <li>
                <Link to="/" className="nav-link">
-                 Home
+                 <button className="btn">Home</button>
                </Link>
              </li>
              <li>
                <Link to="/Search" className="nav-link">
-                 Search
+                <button className="btn">Search</button>
                </Link>
              </li>
            </ul>
