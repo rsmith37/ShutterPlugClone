e60c43c36180e48d42392812fd95db11c1ced456
Richard Smith <rsmith37@ggc.edu>
2019-09-21 17:16:12 -0400

    added logo above header

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -4,28 +4,30 @@ import { Link } from 'react-router-dom';
 
 const Header = props => {
   return (
-    <nav className="navbar sticky-top navbar-expand-sm mb-3 py-0">
-      <div className="logo" style={{ height: '50vh' }}>
-
+    <div>
+      <div className="img-responsive">
+        <img src={require('../../assets/shutter_plug_logo.png')} alt="Shutter Plug Logo"/>
       </div>
-      <div className="container-fluid bg-danger">
-        <h1>Welcome!</h1>
-        <div>
-           <ul className="navbar-nav mr-auto">
-             <li>
-               <Link to="/" className="nav-link">
-                 <button className="btn">Home</button>
-               </Link>
-             </li>
-             <li>
-               <Link to="/Search" className="nav-link">
-                <button className="btn">Search</button>
-               </Link>
-             </li>
-           </ul>
+      <nav className="navbar sticky-top navbar-expand-sm mb-3 py-0">
+        <div className="container-fluid bg-danger">
+          <h1>Shutter Plug</h1>
+          <div>
+            <ul className="navbar-nav mr-auto">
+              <li>
+                <Link to="/" className="nav-link">
+                  <button className="btn">Home</button>
+                </Link>
+              </li>
+              <li>
+                <Link to="/Search" className="nav-link">
+                  <button className="btn">Search</button>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   )
 }
 
