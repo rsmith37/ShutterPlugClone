9dd4310af1c628ca8f5c388983306f9cd0bcca20
rcunico <rcunico@ggc.edu>
2019-09-22 19:52:55 -0400

    Created server.js and started setting up back-end

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+//This is the string used to connect to our MongoDB database
+module.exports = {
+  mongoURI:
+    "mongodb+srv://ryan:shadow123@cluster0-9voe1.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
