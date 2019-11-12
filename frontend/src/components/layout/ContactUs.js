45d8c5af3da2d9997bfe7e40c665df7c62d92f60
Rodney <regulus_2010@yahoo.com>
2019-11-12 10:24:26 -0500

    updated Carousel and condensed landing page

--- frontend/src/components/layout/ContactUs.js
+++ frontend/src/components/layout/ContactUs.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 
 
 export class ContactUs extends Component {
@@ -40,4 +40,4 @@ export class ContactUs extends Component {
     }
 }
 
-export default ContactUs;
+export default ContactUs;*/
