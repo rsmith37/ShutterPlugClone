6706e7ba2ce4ad60f409c9b07ff27219355f72de
Ryan Cunico <rcunico@ggc.edu>
2019-11-20 22:13:37 -0500

    Changed Contact Us email

--- frontend/src/components/layout/Home.js
+++ frontend/src/components/layout/Home.js
@@ -60,6 +60,8 @@ class Home extends Component {
                     <h4>Have questions, offer feedback, or would like to report a bug?{"\n"}</h4>
                     <div className = "text-center px-3">
                         <h5><a href="mailto:cconnersillustrations@hotmail.com">Contact Shutter Plug!</a></h5>
+                        {/* Changed to Shutter Plug Developers email */}
+                        <h5><a href="mailto:shutterplugdevelopers@gmail.com">Contact Shutter Plug!</a></h5>
                     </div>
                 </div>           
             </div>
