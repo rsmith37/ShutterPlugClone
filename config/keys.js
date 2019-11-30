d29943f09ae4326c8a6f5db72a36f29923a37544
Ryan Cunico <rcunico@ggc.edu>
2019-11-30 12:49:02 -0500

    Setting up for Heroku deployment

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-//This is the string used to connect to our MongoDB database
-module.exports = {
-  mongoURI:
-    "mongodb+srv://development:37xFPcAntGCOGmFa@cluster0-bpm7t.mongodb.net/test?retryWrites=true&w=majority",
-  
-  secretOrKey: "37xFPcAntGCOGmFa"
-};
+if(process.env.NODE_ENV === 'production')  {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
