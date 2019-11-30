5634901be6f6654a4fc56b34c89377b23a132eb7
Ryan Cunico <rcunico@ggc.edu>
2019-11-30 13:34:57 -0500

    Server.js

--- server.js
+++ server.js
@@ -35,16 +35,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Passport middleware
 app.use(passport.initialize());
 
+// Passport configuration - passport uses what's called a "strategy"
+require("./config/passport")(passport); // Pass in Passport to the Passport config
+
 //Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
   }
-  
-// Passport configuration - passport uses what's called a "strategy"
-require("./config/passport")(passport); // Pass in Passport to the Passport config
 
 //Use Routes
 app.use("/api/users", users);
